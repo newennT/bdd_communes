02d98e649b24e045ffbb42606e06068cb7bbb572
Newenn Turbiau <84156833+newennT@users.noreply.github.com>
2025-02-25 13:51:28 +0100

    Delete vendor directory

--- vendor/babdev/pagerfanta-bundle/src/Exception/ImmutableViewFactoryException.php
+++
@@ -1,7 +0,0 @@
-<?php declare(strict_types=1);
-
-namespace BabDev\PagerfantaBundle\Exception;
-
-use Pagerfanta\Exception\RuntimeException;
-
-final class ImmutableViewFactoryException extends RuntimeException {}
